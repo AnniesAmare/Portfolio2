3bdf130526935204398f7bebf125108448a1cc8c
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-02 14:04:33 +0100

    https changed to http. And exception handling added to program.cs

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-//builder.Services.AddMvcCore();
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IDataService, DataService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -15,14 +14,13 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 
-/*
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-app.UseRouting();
-*/
+
 
 app.MapControllers();
 app.Run();
