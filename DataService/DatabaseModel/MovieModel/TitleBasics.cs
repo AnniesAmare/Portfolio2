0cfe170bdffd24c33f70b192a9349a4272108510
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-03 21:50:06 +0100

    New branch made

--- DataService/DatabaseModel/MovieModel/TitleBasics.cs
+++ DataService/DatabaseModel/MovieModel/TitleBasics.cs
@@ -11,5 +11,9 @@ namespace DataLayer.DatabaseModel
         public string TConst { get; set; }
         public string? TitleType { get; set; }
         public string? PrimaryTitle { get; set; }
+        public string? OriginalTitle { get; set; }
+        public bool IsAdult { get; set; }
+        public string? StartYear { get; set; }
+        public string? EndYear { get; set; }
     }
 }
