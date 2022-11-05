e19fed9c316875c976290de9618e517e4d140f03
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-05 11:18:22 +0100

    DbSet naming Type fixed to TType

--- DataService/DatabaseModel/MovieModel/Genre.cs
+++ DataService/DatabaseModel/MovieModel/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace DataLayer.DatabaseModel
 {
     public class Genre
     {
+        
         public string? TConst { get; set; }
         public TitleBasic? TitleBasic { get; set; }
         public string? TGenre { get; set; }
