383909f58177eef85bc9437b0e7c13d0006c1961
AnniesAmare <annethearendtorff@gmail.com>
2022-11-05 13:31:03 +0100

    Problems with NameBasicsNconst

---
+++ DataService/DataTransferModel/ActorListElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataTransferModel
+{
+    public class ActorListElement
+    {
+        public string? NConst { get; set; }
+        public string? Name { get; set; }
+        public string? Character { get; set; }
+    }
+
+}
