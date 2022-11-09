285bebb9fc32ae7adbda60016b146612fad8f77d
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-09 11:13:47 +0100

    added titles class

---
+++ DataService/DataTransferModel/Titles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataTransferModel
+{
+    public class Titles
+    {
+        public string? TConst { get; set; }
+        public string? TitleType { get; set; }
+        public string? PrimaryTitle { get; set; }
+        public string? StartYear { get; set; }
+        public bool IsTvShow { get; set; }
+
+    }
+}
