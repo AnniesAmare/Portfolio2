6841a3923f8cb928c0496f1541e2a5f1c9bb8d74
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-10 20:30:22 +0100

    tvShowModel added

---
+++ WebServer/Model/TvShowModel.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.DataTransferModel;
+
+namespace WebServer.Model
+{
+    public class TvShowModel
+    {
+        public string? TConst { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? AiringDate { get; set; }
+        public IList<DirectorListElement>? DirectorList { get; set; }
+        public IList<TvShowListElement>? TvShowContentList { get; set; }
+        public float? Rating { get; set; }
+    }
+}
