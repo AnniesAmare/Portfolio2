86e7863356cb7de823e7652d457738053b2c1006
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-11 14:38:58 +0100

    persons class updated

--- DataService/DataTransferModel/Persons.cs
+++ DataService/DataTransferModel/Persons.cs
@@ -8,6 +8,11 @@ namespace DataLayer.DataTransferModel
 {
     public class Persons
     {
+        public string? NConst { get; set; }
+        public string? Name { get; set; }
+        public string? BirthYear { get; set; }
+        public string? DeathYear { get; set; }
+        public float? AVGRating { get; set; }
     }
 }
 
