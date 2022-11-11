400fb62c1de1011571192b128ed20b12b475d3c0
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-11 14:37:01 +0100

    FINALLY managed to update database so name_basic attribute AVGNameRating not empty

---
+++ DataService/DataTransferModel/Persons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataTransferModel
+{
+    public class Persons
+    {
+    }
+}
+
+
+//name, popularity, movies / Tv-shows (titles) 
