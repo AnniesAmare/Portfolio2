12ead3c40c1f22bcb172a7b8574de6706570296a
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-11-13 05:11:03 +0100

    removed first and unused dataservice and Idataservice

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IDataService, DataService>();
+
 
 //DATASERVICES
 builder.Services.AddSingleton<IDataserviceSpecificPerson, DataserviceSpecificPerson>();
