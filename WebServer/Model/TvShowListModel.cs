1800c2352572cea852d7e13e93a880a485240dda
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-12-18 02:38:34 +0100

    directorList added to specific tvshow

--- WebServer/Model/TvShowListModel.cs
+++ WebServer/Model/TvShowListModel.cs
@@ -7,6 +7,7 @@ namespace WebServer.Model
         public string? Name { get; set; }
         public string? AiringDate { get; set; }
         public IList<TvShowModel>? TvShowContentList { get; set; }
+        public IList<DirectorListElementModel>? DirectorListWithUrl { get; set; }
         public float? Rating { get; set; }
         public string? Url { get; set; }
     }
